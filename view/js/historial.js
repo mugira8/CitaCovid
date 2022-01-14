fa9c9b79f8c758711821b3abeffa18a1aae33fc7
mikelburgoa <m_burgoa@fpzornotza.com>
2022-01-14 09:35:38 +0100

    historial angular

--- view/js/historial.js
+++ view/js/historial.js
@@ -2,7 +2,7 @@ var MyApp = angular.module('miApp', []);
 
 MyApp.controller('miControlador',['$scope','$http', function($scope,$http){
 
-    $http.get('').then(function (response){
+    $http.get('/controller/').then(function (response){
         $scope.lista = response.data;
     });
 
