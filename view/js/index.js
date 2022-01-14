57843cac87537fdcc32d31d9338a81bedf2243de
kevino333 <74492727+kevino333@users.noreply.github.com>
2022-01-14 09:51:26 +0100

    Carrusel index a angular

--- view/js/index.js
+++ view/js/index.js
@@ -8,25 +8,9 @@ myApp.controller("mainController", ["$scope", "$http", function($scope, $http){
 
 $http.get("view/js/articulos.json").then(function(response){
     $scope.articulo = response.data;
+    $scope.imagen = imagenesCarrusel;
 });
 }]);
 
-var estado = "active";
-for (let i = 0; i < imagenesCarrusel.length; i++) {
-if (i>0) { //Pone la clase active solo en el primer item del carrusel
-    estado = ""
-}
-    document.getElementById("myCarouselInner").innerHTML +=`
-    <div class="carousel-item `+ estado +`">
-        <div class="carousel-img"
-            style="background-image: url(`+ imagenesCarrusel[i] +`);">
-        </div>
-    </div>`;
-}
-for (let i = 0; i < imagenesCarrusel.length; i++) {
-    if(i==0){
-        var element = document.getElementById
-    }
-}
 
-var esNuevo = `<span class="badge bg-danger ">New</span>`;
+//var esNuevo = `<span class="badge bg-danger ">New</span>`;
