b4b8b3f40cf7219a39fb89d1776b09f5d9b43c80
mikelburgoa <m_burgoa@fpzornotza.com>
2022-01-14 08:55:36 +0100

    Angular de historial ""terminado""

--- view/js/historial.js
+++ view/js/historial.js
@@ -2,4 +2,8 @@ var MyApp = angular.module('miApp', []);
 
 MyApp.controller('miControlador',['$scope','$http', function($scope,$http){
 
+    $http.get('').then(function (response){
+        $scope.lista = response.data;
+    });
+
 }]);
