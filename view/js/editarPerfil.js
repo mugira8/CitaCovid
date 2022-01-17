cdb82588dbfdbf5fbb7f770b4df9f39bc86fcd2d
mikelburgoa <m_burgoa@fpzornotza.com>
2022-01-17 14:11:21 +0100

    pendiente de cosas

--- view/js/editarPerfil.js
+++ view/js/editarPerfil.js
@@ -3,9 +3,13 @@ var MyApp = angular.module('miApp', []);
 MyApp.controller('miControlador',['$scope','$http', function($scope,$http){
 
     $http.get('controller/').then(function (response){
-        
+
         // $scope.lista = response.data.list;
         
     });
 
+
+    $scope.habilitarEdit=function() {
+        
+    }
 }]);
