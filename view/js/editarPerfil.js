5061522e8b7a704fd1cb19f33295ab3385957d69
mikelburgoa <m_burgoa@fpzornotza.com>
2022-01-17 13:48:55 +0100

    Historial angular ( falta juntarlo al login ) + edit Perfil angular empezando

--- view/js/editarPerfil.js
+++ view/js/editarPerfil.js
@@ -2,9 +2,9 @@ var MyApp = angular.module('miApp', []);
 
 MyApp.controller('miControlador',['$scope','$http', function($scope,$http){
 
-    
-    $http.get('controller/cHistorial.php').then(function (response){
-        $scope.lista = response.data.list;
+    $http.get('controller/').then(function (response){
+        
+        // $scope.lista = response.data.list;
         
     });
 
