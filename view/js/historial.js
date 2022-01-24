06fc7c4326edc319abf27e9dc620ab525885e36f
mikelburgoa <m_burgoa@fpzornotza.com>
2022-01-24 12:37:48 +0100

    guardar cambios

--- view/js/historial.js
+++ view/js/historial.js
@@ -3,6 +3,7 @@ var MyApp = angular.module("myApp", []);
 MyApp.controller('miControlador',['$scope','$http', function($scope,$http){
 
     
+    
     $http.get('controller/cHistorial.php').then(function (response){
         $scope.lista = response.data.list;
         
@@ -22,6 +23,16 @@ MyApp.controller('miControlador',['$scope','$http', function($scope,$http){
         window.location.reload();
     })
     }
+
+    $scope.verHistorial = function() {
+        $scope.ver = "si";
+    }
+
+}]);
+
+MyApp.controller('prueba',['$scope','$http', function($scope,$http){
+
+
 
 }]);
 
