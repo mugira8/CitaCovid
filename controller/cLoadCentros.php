a8aad9641316464dab63f819df98d87800d790ad
Arc042 <j_santisteban@fpzornotza.com>
2022-01-25 13:37:01 +0100

    Update cLoadCentros.php

--- controller/cLoadCentros.php
+++ controller/cLoadCentros.php
@@ -2,11 +2,22 @@
 
 include_once '../model/centrosModel.php';
 
-$data=json_decode(file_get_contents("php://input"),true);
-
-$cod_centro=$data['cod_centro'];
+$response=array();
 
 $centro=new centrosModel();
+
+$response['list']= $centro->setList();
+$response['error']='no error';
+
+echo json_encode($response);
+
+unset($response);
+
+// $data=json_decode(file_get_contents("php://input"),true);
+
+// $cod_centro=$data['cod_centro'];
+
+// $centro=new centrosModel();
 
 
 ?>
