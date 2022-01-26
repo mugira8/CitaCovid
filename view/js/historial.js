d61b0ab671bfb0d2fe5f0fe77445e33c0e786ab2
mikelburgoa <m_burgoa@fpzornotza.com>
2022-01-26 12:45:48 +0100

    arreglao

--- view/js/historial.js
+++ view/js/historial.js
@@ -1,6 +1,14 @@
 var MyApp = angular.module("myApp", []);
 
 MyApp.controller('miControlador',['$scope','$http',  function($scope,$http){
+
+    $http.get('controller/cSessionVarsView.php').then(function (response) { 
+
+        objPaciente = response.data;
+
+        $scope.paciente = objPaciente.paciente;
+    });
+
     sessionVarsView()
      function sessionVarsView() {
         var url = "controller/cSessionVarsView.php";
@@ -25,22 +33,6 @@ MyApp.controller('miControlador',['$scope','$http',  function($scope,$http){
                 
                 console.log(result)
                 $scope.lista = result.list;
-    
-            });
-
-            url= 'controller/cLoadPacientes.php';
-            data = {"TIS": result.paciente.tis};
-            fetch(url, {
-                method: 'POST',
-                body: JSON.stringify(data),
-                headers: { 'Content-Type': 'application/json' }
-            }).then(res => res.json()).then(result => {
-                console.log('session result', result)
-                console.log(window.location.href)
-                objPaciente = result;
-                
-                console.log(result)
-                $scope.lista2 = result.list;
     
             });
         });
@@ -108,4 +100,38 @@ function changeFitx(action) {
 			  }  
 		  }
 	}
+}
+
+$("#btnEnviar").on('click', execUpdate);
+
+function execUpdate() {
+    
+    var nombre = $("#nombre").val();
+    var apellido = $("#apellido").val();
+
+    var url = "controller/cPacienteUpdate.php";
+    var data = {'nombre': nombre, 'apellido': apellido, 'filename': filename, 'savedFileBase64': savedFileBase64};
+
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data), 
+        headers:{'Content-Type': 'application/json'} 
+        })
+        .then(res => res.json()).then(result => {
+	
+            console.log(result.error);
+            alert(result.error);
+            $("#update").style.display="none";
+         
+         var inputs = document.querySelectorAll("#update input");
+         for (let i = 0; i < inputs.length; i++) {
+             inputs[i].value = "";
+         }
+         var imgs=document.querySelectorAll("#update img");
+         for (let i = 0; i < imgs.length; i++) {
+             imgs[i].setAttribute('src','');
+         }
+        }
+ )
+ .catch(error => console.error('Error status:', error));
 }
